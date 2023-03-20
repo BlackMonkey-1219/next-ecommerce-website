fd82a57ff9b3158b47e621d00367c7f684b08d38
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-03-20 10:15:25 +0530

    ProductVarient no longer stores in db directly

--- src/models/ProductVarient.ts
+++ src/models/ProductVarient.ts
@@ -1,4 +1,3 @@
-import getDatabase from '@/lib/getDBClient';
 import { ObjectId } from 'mongodb';
 
 class ProductVarient {
@@ -12,7 +11,7 @@ class ProductVarient {
     this.varientDescription = description;
     this.varientPrice = price;
 
-    this._id = id ?? undefined;
+    this._id = id ?? new ObjectId();
   }
 
   get Id() {
@@ -38,75 +37,6 @@ class ProductVarient {
   }
   get VarientPrice() {
     return this.varientPrice;
-  }
-
-  async pushToDatabase() {
-    try {
-      if (this._id) {
-        throw new Error('This is document already exists on the database.');
-      }
-
-      const db = await getDatabase();
-      const collection = db.collection('product_varients');
-      return await collection.insertOne(this);
-    } catch (error) {
-      console.log('[-] COULD NOT PUSH PRODUCT VARIENT TO THE DATABASE...');
-      console.log('=====================ERROR=====================');
-      console.log(error);
-      console.log('===============================================');
-    }
-  }
-
-  async saveChanges() {
-    try {
-      if (!this._id) {
-        throw new Error('This is a new document. Try pushing to the database.');
-      }
-
-      const db = await getDatabase();
-      const collection = db.collection('product_varients');
-      collection.updateOne({ id: this._id }, this);
-    } catch (error) {
-      console.log('[-] COULD NOT SAVE CHANGES TO THE DATABASE...');
-      console.log('=====================ERROR=====================');
-      console.log(error);
-      console.log('===============================================');
-    }
-  }
-
-  static async findById(id: ObjectId) {
-    try {
-      const db = await getDatabase();
-      const collection = db.collection('product_varients');
-      const doc = await collection.findOne({ _id: id });
-
-      if (doc) {
-        return new ProductVarient(
-          doc.varientName,
-          doc.varientDescription,
-          doc.varientPrice,
-          doc._id
-        );
-      } else {
-        return null;
-      }
-    } catch (error) {
-      console.log('=====================ERROR=====================');
-      console.log(error);
-      console.log('===============================================');
-    }
-  }
-
-  static async deleteById(id: ObjectId) {
-    try {
-      const db = await getDatabase();
-      const collection = db.collection('product_varient');
-      return await collection.deleteOne({ _id: id });
-    } catch (error) {
-      console.log('=====================ERROR=====================');
-      console.log(error);
-      console.log('===============================================');
-    }
   }
 }
 
