b9ffe4435b9e60ee9857a2f00c7752022149d61b
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-03-21 06:40:16 +0530

    Created remove_shop endpoint

---
+++ src/pages/api/shop/remove_shop.ts
@@ -0,0 +1,53 @@
+import Seller from '@/models/Seller';
+import Shop from '@/models/Shop';
+import startSection from '@/utility/logToTerminal';
+import { ObjectId } from 'mongodb';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function Handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  startSection('REMOVE SHOP');
+  console.log('REQ BODY: ', req.body, '\n');
+
+  try {
+    const { shop_id } = req.body;
+
+    // GRAB SHOP
+    const shop = await Shop.findById(ObjectId.createFromHexString(shop_id));
+
+    if (shop) {
+      const sellerId = shop.OwnerId;
+      console.log('[i] SHOP OWNER ID: ', sellerId, '\n');
+
+      // REMOVE SHOP
+      const shopRemoveResult = await Shop.deleteById(
+        ObjectId.createFromHexString(shop_id)
+      );
+      console.log(shopRemoveResult, '\n');
+
+      // GRAB SELLER
+      const seller = await Seller.findById(sellerId);
+      console.log(seller, '\n');
+
+      // REMOVE SHOP FROM SELLER
+      if (seller) {
+        seller.ShopID = undefined;
+        const sellerProfileUpdateResult = await seller.saveChanges();
+        console.log(sellerProfileUpdateResult, '\n');
+      } else {
+        throw new Error(`[-] COULD NOT FIND A SELLER WITH ID: ${sellerId}`);
+      }
+    } else {
+      throw new Error(`[-] COULD NOT FIND THE SHOP WITH ID: ${shop_id}`);
+    }
+
+    console.log('[+] SHOP REMOVED SUCCESSFULLY...');
+    res.json({ message: '[+] SHOP REMOVED SUCCESSFULLY...' });
+  } catch (error) {
+    console.log('[-] COULD NOT REMOVE THE SHOP...');
+    console.log(error, '\n');
+    res.json({ message: '[-] COULD NOT REMOVE THE SHOP...' });
+  }
+}
