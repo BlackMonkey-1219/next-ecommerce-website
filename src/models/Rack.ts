554a331acb3a434711441d9bd161c9bf013c7a15
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-03-21 05:48:05 +0530

    Removed db functions

--- src/models/Rack.ts
+++ src/models/Rack.ts
@@ -1,19 +1,22 @@
-import getDatabase from '@/lib/getDBClient';
 import { ObjectId } from 'mongodb';
 
 class Rack {
-  private id: ObjectId | undefined;
-  private rackName: string = '';
-  private rackItems: Array<ObjectId> = [];
+  private _id: ObjectId | undefined;
+  private rackName: string;
+  private rackItems: Array<ObjectId>;
 
-  constructor(name: string, rackItems: Array<ObjectId>, id: ObjectId) {
+  constructor(
+    name: string,
+    rackItems: Array<ObjectId> = [],
+    id: ObjectId | undefined = undefined
+  ) {
     this.rackName = name;
     this.rackItems = rackItems;
-    this.id = id ?? undefined;
+    this._id = id ?? new ObjectId();
   }
 
   get Id() {
-    return this.id;
+    return this._id;
   }
 
   set RackName(name: string) {
@@ -28,70 +31,6 @@ class Rack {
   }
   get RackItems() {
     return this.rackItems;
-  }
-
-  async pushToDatabase() {
-    try {
-      if (this.id) {
-        throw new Error('This is document already exists on the database.');
-      }
-
-      const db = await getDatabase();
-      const collection = db.collection('racks');
-      return await collection.insertOne(this);
-    } catch (error) {
-      console.log('[-] COULD NOT PUSH NEW RACK TO THE DATABASE...');
-      console.log('=====================ERROR=====================');
-      console.log(error);
-      console.log('===============================================');
-    }
-  }
-
-  async saveChanges() {
-    try {
-      if (!this.id) {
-        throw new Error('This is a new document. Try pushing to the database.');
-      }
-
-      const db = await getDatabase();
-      const collection = db.collection('racks');
-      collection.updateOne({ _id: this.id }, this);
-    } catch (error) {
-      console.log('[-] COULD NOT SAVE CHANGES TO THE DATABASE...');
-      console.log('=====================ERROR=====================');
-      console.log(error);
-      console.log('===============================================');
-    }
-  }
-
-  static async findById(id: ObjectId) {
-    try {
-      const db = await getDatabase();
-      const collection = db.collection('racks');
-      const doc = await collection.findOne({ _id: id });
-
-      if (doc) {
-        return new Rack(doc.rackName, doc.rackItems, doc._id);
-      } else {
-        return null;
-      }
-    } catch (error) {
-      console.log('=====================ERROR=====================');
-      console.log(error);
-      console.log('===============================================');
-    }
-  }
-
-  static async deleteById(id: ObjectId) {
-    try {
-      const db = await getDatabase();
-      const collection = db.collection('racks');
-      return await collection.deleteOne({ _id: id });
-    } catch (error) {
-      console.log('=====================ERROR=====================');
-      console.log(error);
-      console.log('===============================================');
-    }
   }
 }
 
