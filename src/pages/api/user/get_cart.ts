ed9320d22715e586724ebbe5df01c1770432a562
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-03-22 11:30:50 +0530

    Added get_cart endpoint

---
+++ src/pages/api/user/get_cart.ts
@@ -0,0 +1,46 @@
+import Cart from '@/models/Cart';
+import startSection, { endSection } from '@/utility/logToTerminal';
+import { ObjectId } from 'mongodb';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function Handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  startSection('GET CART');
+  console.log('REQ BODY: ', req.body);
+
+  try {
+    const { user_id } = req.body;
+
+    // FETCH CART
+    const cart = Cart.findByCartOwnerId(ObjectId.createFromHexString(user_id));
+
+    if (cart) {
+      // SEND CART
+      res.json({
+        message: '[+] CART FETCHED SUCCESSFULLY...',
+        data: { user_cart: cart },
+      });
+      console.log('[+] CART FETCHED SUCCESSFULLY...');
+    } else {
+      // CREATE NEW CART
+      console.log('[+] CREATING A NEW CART FOR THE NEW USER...');
+      const newCart = new Cart(user_id);
+      const cartSaveResult = newCart.pushToDatabase();
+      console.log(cartSaveResult);
+
+      // SEND NEW CART
+      res.json({
+        message: '[+] CART CREATED FOR THE NEW USER...',
+        data: { user_cart: newCart },
+      });
+    }
+  } catch (error) {
+    console.log('[-] COULD NOT FETCH THE CART...');
+    console.log(error, '\n');
+    res.json({ message: '[-] COULD NOT FETCH YOUR CART...' });
+  }
+
+  endSection();
+}
