9d82c42cf130b5e3a6dd109430865c56bf12559c
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-03-23 09:41:23 +0530

    Added add_review endpoint

---
+++ src/pages/api/review/add_review.ts
@@ -0,0 +1,37 @@
+import ProductReview from '@/models/ProductReview';
+import startSection, { endSection } from '@/utility/logToTerminal';
+import { ObjectId } from 'mongodb';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function Handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  startSection('ADD REVIEW');
+  console.log('REQ BODY: ', req.body);
+
+  try {
+    const { user_id, product_id, rating, review } = req.body;
+    // CREATE NEW REVIEW
+
+    const productReview = new ProductReview(
+      ObjectId.createFromHexString(user_id),
+      ObjectId.createFromHexString(product_id),
+      parseInt(rating),
+      review
+    );
+
+    //  PUSH TO DATABASE
+    const reviewSaveResult = await productReview.pushToDatabase();
+    console.log(reviewSaveResult);
+
+    console.log('[+] REVIEW CREATED SUCCESSFULLY...');
+    res.json({ message: '[+] REVIEW POSTED SUCCESSFULLY...' });
+  } catch (error) {
+    console.log('[-] COULD NOT POST THE REVIEW...');
+    console.log(error, '\n');
+    res.json({ message: '[-] COULD NOT POST THE REVIEW...' });
+  }
+
+  endSection();
+}
