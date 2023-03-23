ad3b461b1bc5112e817e686bf7016ef3349f4845
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-03-23 06:54:14 +0530

    Added create_transaction endpoint

---
+++ src/pages/api/transaction/create_transaction.ts
@@ -0,0 +1,35 @@
+import TransactionRecord from '@/models/TransactionRecord';
+import startSection, { endSection } from '@/utility/logToTerminal';
+import { ObjectId } from 'mongodb';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function Handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  startSection('CREATE TRANSACTION');
+  console.log('REQ BODY: ', req.body);
+
+  try {
+    const { user_id, shop_id, products } = req.body;
+
+    // CREATE NEW TRANSACTION
+    const transactionRecord = new TransactionRecord(
+      ObjectId.createFromHexString(user_id),
+      ObjectId.createFromHexString(shop_id),
+      products
+    );
+    // SAVE TO DATABASE
+    const recordSaveResult = await transactionRecord.pushToDatabase();
+    console.log(recordSaveResult);
+
+    console.log('[+] TRANSACTION RECORD CREATED SUCCESSFULLY...');
+    res.json({ message: '[+] TRANSACTION RECORD CREATED SUCCESSFULLY...' });
+  } catch (error) {
+    console.log('[-] COULD NOT CREATE TRANSACTION...');
+    console.log(error, '\n');
+    res.json({ message: '[-] COULD NOT CREATE TRANSACTION...' });
+  }
+
+  endSection();
+}
