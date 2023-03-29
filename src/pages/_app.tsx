c25a1e24424c26c993a9829afddd43e1f2fc22b4
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-03-30 04:34:11 +0530

    Wrapped App with icon context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+import { IconContext } from 'react-icons';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <IconContext.Provider value={{ size: '1.5rem' }}>
+        <Component {...pageProps} />;
+      </IconContext.Provider>
+    </SessionProvider>
+  );
 }
