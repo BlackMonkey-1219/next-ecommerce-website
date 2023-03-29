699ce419fc5ef85f3483a2d02f16811c002fd887
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-03-30 04:37:39 +0530

    Created home hero section category list

---
+++ src/components/HomeHeroCategoryList/HomeHeroCategoryList.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+function HomeHeroCategoryList() {
+  return (
+    <ul
+      className={
+        'w-fit h-full p-[0.5rem] flex flex-col items-start justify-start gap-[1rem] whitespace-nowrap'
+      }>
+      <li
+        className={
+          'w-full h-fit hover:bg-blue-100 px-[0.5em] py-[0.25em] rounded-md'
+        }>
+        Vehicle Accessories
+      </li>
+      <li
+        className={
+          'w-full h-fit hover:bg-blue-100 px-[0.5em] py-[0.25em] rounded-md'
+        }>
+        Fashion & Beauty
+      </li>
+      <li
+        className={
+          'w-full h-fit hover:bg-blue-100 px-[0.5em] py-[0.25em] rounded-md'
+        }>
+        Clothes
+      </li>
+      <li
+        className={
+          'w-full h-fit hover:bg-blue-100 px-[0.5em] py-[0.25em] rounded-md'
+        }>
+        Home Decor
+      </li>
+      <li
+        className={
+          'w-full h-fit hover:bg-blue-100 px-[0.5em] py-[0.25em] rounded-md'
+        }>
+        Garden
+      </li>
+      <li
+        className={
+          'w-full h-fit hover:bg-blue-100 px-[0.5em] py-[0.25em] rounded-md'
+        }>
+        Tools
+      </li>
+      <li
+        className={
+          'w-full h-fit hover:bg-blue-100 px-[0.5em] py-[0.25em] rounded-md'
+        }>
+        Toys
+      </li>
+      <li
+        className={
+          'w-full h-fit hover:bg-blue-100 px-[0.5em] py-[0.25em] rounded-md'
+        }>
+        Sports & Outdoor
+      </li>
+      <li
+        className={
+          'w-full h-fit hover:bg-blue-100 px-[0.5em] py-[0.25em] rounded-md'
+        }>
+        Animals & Pets
+      </li>
+      <li
+        className={
+          'w-full h-fit hover:bg-blue-100 px-[0.5em] py-[0.25em] rounded-md'
+        }>
+        Electronics
+      </li>
+    </ul>
+  );
+}
+
+export default HomeHeroCategoryList;
