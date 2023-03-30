8a5f9941dd7659c8c4aea0c8eff8b709d027b726
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-03-30 14:05:46 +0530

    Created hero notice panel

---
+++ src/components/HeroNoticePanel/HeroNoticePanel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginPanel from '../LoginPanel/LoginPanel';
+
+function HeroNoticePanel() {
+  return (
+    <div className={'basis-[20%]'}>
+      <LoginPanel />
+    </div>
+  );
+}
+
+export default HeroNoticePanel;
