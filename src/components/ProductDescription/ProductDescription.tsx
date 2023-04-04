d6e99877294acaafcbf87d72643610564a973777
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-04-04 11:36:27 +0530

    Created description card:incomplete

---
+++ src/components/ProductDescription/ProductDescription.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function ProductDescription() {
+  return (
+    <article className={'w-full h-fit p-[1rem] bg-white'}>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum quia
+        ea, eligendi blanditiis similique repudiandae perspiciatis earum
+        laudantium sapiente alias assumenda modi aspernatur perferendis magnam!
+        Ipsa, dolorum a, quidem hic pariatur eaque, dolorem cupiditate totam
+        velit delectus soluta consequuntur incidunt ipsum sunt harum ab facilis.
+        Ratione numquam quos, similique atque obcaecati nihil quia voluptas
+        voluptate quidem quis doloremque, hic odit pariatur nam, corrupti
+        labore. Alias ut nihil molestias vitae atque, illo quidem voluptatibus
+        voluptate pariatur dolorem soluta nisi. Sapiente amet excepturi illo
+        quasi, commodi nobis suscipit veniam ab fugiat obcaecati iusto, sint
+        similique quod placeat rerum tempore, inventore architecto vel.
+      </p>
+    </article>
+  );
+}
+
+export default ProductDescription;
